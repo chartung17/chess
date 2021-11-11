cb077471c4e3d7b10b6fb451ae69cfab4bd3da3a
chartung17 <57280457+chartung17@users.noreply.github.com>
2021-11-10 18:03:46 -0700

    testing square selection

--- src/main/java/ui/js/JavaScriptUserInterface.java
+++ src/main/java/ui/js/JavaScriptUserInterface.java
@@ -17,6 +17,10 @@ public class JavaScriptUserInterface extends UserInterface {
 	private byte[][] board = new byte[8][8];
 	private int[] selectedSquare = null;
 	private boolean handlingButton = false;
+	private static final String ILLEGAL = "{\"status\":405,\"message\":\"Illegal move\"}";
+	private static final String DEFAULT = "[\"Resign\",\"Offer Draw\"]";
+	private static final String YES_NO = "[\"Yes\",\"No\"]";
+	private static final String PROMOTION = "[\"Queen\",\"Rook\",\"Knight\",\"Bishop\"";
 
 	@Override
 	protected void updateBoard(byte[][] board) {
@@ -42,6 +46,8 @@ public class JavaScriptUserInterface extends UserInterface {
 	public static String handleSelectedSquare(@PathVariable int row, @PathVariable int col,
 			HttpSession session) {
 		JavaScriptUserInterface ui = getUI(session);
+		if (ui.handlingButton || (row < 0) || (row >= 8) || (col < 0) || (col >= 8))
+			return ILLEGAL;
 		int result = ui.mediator.handleSelectedSquare(row, col);
 		if ((result == Board.SELECT) || (result == Board.PROMOTE)) {
 			ui.selectedSquare = new int[] { row, col };
@@ -67,8 +73,17 @@ public class JavaScriptUserInterface extends UserInterface {
 				movesStr.setCharAt(8 * move[0] + move[1], 'X');
 			}
 		}
+		String buttons;
+		if (ui.isPromotion) {
+			buttons = PROMOTION;
+		} else if (ui.isGameOver || ui.handlingButton) {
+			buttons = YES_NO;
+		} else {
+			buttons = DEFAULT;
+		}
 		return "{\"status\":200,\"board\":\"" + boardStr.toString() + "\",\"moves\":\""
-				+ movesStr.toString() + "\"}";
+				+ movesStr.toString() + "\",\"message1\":" + ui.messageLine1 + "\",\"message2\":"
+				+ ui.messageLine2 + "\",\"buttons\":" + buttons + "}";
 	}
 
 	public static void main(String[] args) {
